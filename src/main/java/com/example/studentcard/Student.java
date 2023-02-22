f964e2bd44bc746360c58310121ea559bcc1243f
evanhinde <200529232@student.georgianc.on.ca>
2023-02-22 12:28:09 -0500

    Final working commit

--- src/main/java/com/example/studentcard/Student.java
+++ src/main/java/com/example/studentcard/Student.java
@@ -6,20 +6,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 public class Student {
-
+    /**
+     * These are instance variables that are private and stored
+     */
     private String firstName;
     private String lastName;
     private int studentNumber;
     private List<String> activities;
+    private Image image;
 
-    public Student (String firstName, String lastName, int studentNumber, List<String> activities)
+    /**
+     * This is a constructor that's called when creating an instance of an object
+     * @param firstName
+     * @param lastName
+     * @param studentNumber
+     * @param activities
+     * @param image
+     */
+    public Student (String firstName, String lastName, int studentNumber, List<String> activities, Image image)
     {
         setFirstName(firstName);
         setLastName(lastName);
         setStudentNumber(studentNumber);
         setActivities(activities);
+        setImage(image);
     }
 
+    /**
+     * These are get methods that return the instance variables when called
+     * @return
+     */
     public String getFirstName()
     {
         return firstName;
@@ -28,34 +44,54 @@ public class Student {
     {
         return lastName;
     }
-
     public int getStudentNumber()
     {
         return studentNumber;
     }
-
     public List<String> getActivities() {
         return activities;
     }
+    public Image getImage()
+    {
+        return image;
+    }
 
+    /**
+     * This is a set method that validates the first name so that
+     * it is switched to capital letter first and throw an illegal exception
+     * if it is not at least two letters long
+     * @param firstName
+     */
     public void setFirstName(String firstName){
-        firstName = firstName.toUpperCase();
-
         if (firstName.length() < 2) {
             throw new IllegalArgumentException("Names must be at least 2 letters long");
         }
+        firstName = firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase();
+
         this.firstName = firstName;
     }
 
+    /**
+     * This is a set method that validates the last name so that
+     * it is switched to capital letter first and throw an illegal exception
+     * if it is not at least two letters long
+     * @param lastName
+     */
     public void setLastName(String lastName){
-        lastName = lastName.toUpperCase();
 
         if (lastName.length() < 2) {
             throw new IllegalArgumentException("Names must be at least 2 letters long");
         }
+        lastName = lastName.substring(0,1).toUpperCase() + lastName.substring(1).toLowerCase();
+
         this.lastName = lastName;
     }
 
+    /**
+     * This is a set method for the student number that validates a certain range
+     * that it should be in, or it will throw an IAE
+     * @param studentNumber
+     */
     public void setStudentNumber(int studentNumber){
         if (studentNumber < 10000000 || studentNumber > 999999999) {
             throw new IllegalArgumentException("Not valid number");
@@ -63,31 +99,24 @@ public class Student {
         this.studentNumber = studentNumber;
     }
 
+    /**
+     * This is a set method for activities that assigns it to the activities list
+     * @param activities
+     */
     public void setActivities(List<String>activities) {
         this.activities = activities;
     }
 
-    @Override
-    public String toString()
-    {
-        return firstName + lastName;
+    /**
+     * This is a set method for the image that assigns an image to the image variable
+     * @param image
+     */
+    public void setImage(Image image){
+        this.image = image;
     }
-
-    public Image getImage()
-    {
-        String pathName = "images/joseph-gonzalez-iFgRcqHznqg-unsplash.jpg" + firstName + lastName + studentNumber + activities;
-        return new Image(Student.class.getResourceAsStream(pathName));
-    }
-
-
-
-
-
 
 
 
 
 
 }
-
-
--- src/main/java/com/example/studentcard/StudentCardController.java
+++ src/main/java/com/example/studentcard/StudentCardController.java
@@ -5,10 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -19,6 +21,9 @@ public class StudentCardController implements Initializable {
     private ImageView image;
 
     @FXML
+    private ListView<String> list;
+
+    @FXML
     private Label nameLabel;
 
     @FXML
@@ -27,20 +32,97 @@ public class StudentCardController implements Initializable {
     @FXML
     private Label studentNumberLabel;
 
-    @FXML
-    void next(ActionEvent event) {
 
-    }
+    /**
+     * This is creating an array list called students to be filled with
+     * Student objects (Each made up of all our instance variables)
+     */
+    private Student[] students;
 
+    /**
+     * This creates an index of the array list
+     */
+    private int index;
+
+    /**
+     * This method is where the activities list is populated with items,
+     * and the students array is populated with student instances
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<String> activities = Arrays.asList("Golf", "Football", "Baseball");
-        Student student = new Student("Joe", "Smith", 200529232, activities);
-        image.setImage(new Image(Student.class.getResourceAsStream("images/joseph-gonzalez-iFgRcqHznqg-unsplash.jpg")));
-        nameLabel.setText(student.getFirstName() + " " + student.getLastName());
-        studentNumberLabel.setText(String.valueOf(student.getStudentNumber()));
+        List<String> activities1 = Arrays.asList("Golf", "Football", "Baseball");
+        List<String> activities2 = Arrays.asList("Swimming", "Skiing", "Basketball");
+        List<String> activities3 = Arrays.asList("Hiking", "Music", "School");
+        students = new Student[]{
+                new Student("joe", "smith", 200529232, activities1, new Image(Student.class.getResourceAsStream("images/joseph-gonzalez-iFgRcqHznqg-unsplash.jpg"))),
+                new Student("mike", "wilson", 200528373, activities2, new Image(Student.class.getResourceAsStream("images/jurica-koletic-7YVZYZeITc8-unsplash.jpg"))),
+                new Student("sam", "johnson", 200293992, activities3, new Image(Student.class.getResourceAsStream("images/podpros-GEnCnYhA1J4-unsplash.jpg")))
+        };
+
+        /**
+         * This is to set the index of the array to 0
+         */
+        index = 0;
+
+        /**
+         * This is using a method to show a student card with info
+         * and image
+         */
+        showStudent();
+    }
+
+    /**
+     * This method uses the nextButton in the view to move through student
+     * objects in the array (going back to the first image when
+     * all objects have been clicked through)
+     * @param event
+     */
+    @FXML
+    void next(ActionEvent event) {
+        if(index != 3) {
+            showStudent();
+            index = (index + 1);
+        }
+        else{
+            index=0;
+        }
+    }
+
+    /**
+     * This showStudent method displays the student instances depending
+     * on where we are in the index of the students array in the view
+     */
+    private void showStudent() {
+        /**
+         * The displayStudent finds what index we are at in the array
+         */
+        Student displayStudent = students [index];
+        /**
+         * This displays the image of the displayStudent in the imageView
+         */
+        image.setImage(displayStudent.getImage());
+        /**
+         * This displays the first name and last name of the displayStudent
+         * with a space between them in the label of the view
+         */
+        nameLabel.setText(displayStudent.getFirstName() + " " + displayStudent.getLastName());
+        /**
+         * This displays the student number to its corresponding label in the view with
+         * the String.valueOf converting it to a string with a value of the int studentNumber
+         */
+        studentNumberLabel.setText(String.valueOf(displayStudent.getStudentNumber()));
+        /**
+         * This getItems() method gets all the current items in the activities list and
+         * replaces them with setAll() to the activities of the displayStudent
+         */
+        list.getItems().setAll(displayStudent.getActivities());
     }
 
 }
+
+
+
 
 
